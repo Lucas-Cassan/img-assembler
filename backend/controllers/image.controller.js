1bfef1168fb1072ab82091767d1320d23947cf49
Guillaume Gruber <lagrube1@gmail.com>
2022-02-22 12:36:40 +0100

    first commit

--- backend/controllers/image.controller.js
+++ backend/controllers/image.controller.js
@@ -1,7 +1,12 @@
 const multer = require("multer");
 
 exports.mergeImage = async (req, res, next) => {
-  // console.log(Buffer.from(req.file, "base64"));
+  let image = req.body;
+  // image = JSON.stringify(image).replace("}", " ").split(",");
+  image = JSON.stringify(image).replace("}", " ").replace("{", " ");
+  console.log(image);
+  // console.log(image[1]);
+  // console.log(Buffer.from(image[1], "base64"));
   let storage = multer.diskStorage({
     destination: function (req, file, cb) {
       cb(null, "public");
@@ -18,6 +23,6 @@ exports.mergeImage = async (req, res, next) => {
     } else if (err) {
       return res.status(500).json(err);
     }
-    return res.status(200).send(req.file);
+    return res.status(200).send(image);
   });
 };
