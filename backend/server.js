682e9f9c9112c2eafa2c4b605829489bf51ead58
mehdi <mehdi.lamtargi@etudiant.univ-reims.fr>
2022-02-23 09:51:31 +0100

    taille image

--- backend/server.js
+++ backend/server.js
@@ -24,8 +24,14 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 // Body-parser
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(
+  bodyParser.urlencoded({
+    parameterLimit: 10000,
+    extended: true,
+    limit: "50mb",
+  }),
+);
 
 // Routes
 app.use("/img", imgRoute);
