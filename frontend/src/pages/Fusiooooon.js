4762cbe1a3b2b7a2678119c617ab8842620a86ee
Guillaume Gruber <lagrube1@gmail.com>
2022-02-24 09:13:39 +0100

    add auto install

--- frontend/src/pages/Fusiooooon.js
+++ frontend/src/pages/Fusiooooon.js
@@ -1,23 +1,25 @@
 import React, { createRef, useState } from "react";
-import axios from "axios";
-import mergeImages from "merge-images";
 import styled from "styled-components";
 import { Rnd } from "react-rnd";
-import StyledRect from "react-resizable-rotatable-draggable";
 import { useScreenshot } from "use-react-screenshot";
 import { saveAs } from "file-saver";
+import { useEffect } from "react";
 
 const Fusiooooon = () => {
-  const [imgSrc, setImgSrc] = useState(null);
   const [file1, setFile1] = useState(null);
   const [file2, setFile2] = useState(null);
   const ref = createRef(null);
   const [image, takeScreenshot] = useScreenshot();
 
-  function downloadimage(){
+  useEffect(() => {
+    if (image) {
+      saveAs(image, "MicPicture");
+    }
+  }, [image]);
+
+  const downloadimage = async () => {
     takeScreenshot(ref.current);
-    saveAs(image, "MicPicture");
-  }
+  };
 
   const [position, setPosition] = useState({
     x: 0,
@@ -66,55 +68,48 @@ const Fusiooooon = () => {
       y,
     }));
   }
-  
 
   return (
     <>
       <Container ref={ref} id="image" style={{ textAlign: "center" }}>
-        {imgSrc === null ? (
-          <>
-            <div style={{ height: "100%" }}>
-              <img
-                src={file1}
-                alt=""
-                style={{
-                  maxWidth: "100%",
-                  borderRadius: "3px",
-                  maxHeight: "100%",
-                  position: "relative",
-                  top: " 50%",
-                  bottom: "50%",
-                  transform: " translate(0%, -50%)",
-                }}
-                id="FirstImg"
-              />
-              <StyledRnd
-                style={{
-                  width: "33.33%",
-                  position: "absolute",
-                  left: "40%",
-                  top: "40%",
-                  transform: "translate(0%, -20%)!important",
-                }}
-                default={position}
-                onResize={onResize}
-                onDragStop={onDragStop}
-                bounds="parent"
-                lockAspectRatio={true}
-              >
-                <Image
-                  style={{
-                    backgroundImage: `url(` + file2 + `)`,
-                  }}
-                >
-                  {/* {JSON.stringify(position, null, 2)} */}
-                </Image>
-              </StyledRnd>
-            </div>
-          </>
-        ) : (
-          <img src={imgSrc} alt="" className="img" />
-        )}
+        <div style={{ height: "100%" }}>
+          <img
+            src={file1}
+            alt=""
+            style={{
+              maxWidth: "100%",
+              borderRadius: "3px",
+              maxHeight: "100%",
+              position: "relative",
+              top: " 50%",
+              bottom: "50%",
+              transform: " translate(0%, -50%)",
+            }}
+            id="FirstImg"
+          />
+          <StyledRnd
+            style={{
+              width: "33.33%",
+              position: "absolute",
+              left: "40%",
+              top: "40%",
+              transform: "translate(0%, -20%)!important",
+            }}
+            default={position}
+            onResize={onResize}
+            onDragStop={onDragStop}
+            bounds="parent"
+            lockAspectRatio={true}
+          >
+            <Image
+              style={{
+                backgroundImage: `url(` + file2 + `)`,
+              }}
+            >
+              {/* {JSON.stringify(position, null, 2)} */}
+            </Image>
+          </StyledRnd>
+        </div>
       </Container>
       <div id="form">
         <form action="">
